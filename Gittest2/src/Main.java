ff10c2fa0194bfd27c2810bb35718d8ddc23245d
Administrator <Administrator@User -2022TSFVF>
2022-09-08 15:26:37 +0900

    non-fast forward commit

--- Gittest2/src/Main.java
+++ Gittest2/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("gittest2");
 		System.out.println("git commit again");
+		System.out.println("non-fast forward");
 	}
 
 }
